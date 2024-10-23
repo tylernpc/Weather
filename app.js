ee1b707398dc1f7e3bd42831a8d46c8a4143589c
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-10-23 10:15:53 -0500

    Good checkmark on progress, should be good to go in no time

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
+async function getUserLocation() {
+  let userAddress = document.getElementById("userInputAddress").value;
+  let userPostal = document.getElementById("userInputZip").value;
+  // 1 | create api string
+  let apiString = `https://geocoder.ca/?locate=${userAddress},+${userPostal}&geoit=XML&json=1`;
+
+  // 2 | make connection and wait for response
+  let response = await fetch(apiString);
+  let jsonData = await response.json();
+
+  // 3 | do stuff with json below
+  let latitude = jsonData["latt"];
+  let longitude = jsonData["longt"];
+
+  // 4 | pass off info to getWeatherInfo()
+  return [latitude, longitude];
+}
+
 async function getWeatherInfo() {
-    let apiString = `https://api.weather.gov/points/${lat},${lon}`;
-    let response = await fetch(apiString);
-    let jsonData = await response.json();
+  getUserLocation();
+  // 1 | create api string
+  let apiString = `https://api.weather.gov/points/${latitude},${longitude}`;
 
-    // do stuff with json below
+  // 2 | make connection and wait for response
+  let response = await fetch(apiString);
+  let jsonData = await response.json();
 
-    // 
+  // 3 | do stuff with json below
+  document.getElementById("myTemp").innerHTML +=
+    jsonData.properties.periods[i].temperature +
+    " degrees Farenheit" +
+    "<br><br>";
+  //
 }
 
-function getUserLocation() {
-    return document.getElementById('userInputLocation').value;
-}
+// // 40.81084153320897, -96.69021919814986 | DONT PANIC LABS COORDS
 
-
+// // https://geocoder.ca/?locate=1221+n44th+st,+68503&geoit=XML&json=1
